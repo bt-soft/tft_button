b5df77f8da191066d8fc93a96532a98ad30eab63
bt-soft <bt-soft@freemail.hu>
2025-03-02 20:27:03 +0100

    Update button Y position for consistent layout

--- tft_button.ino
+++ tft_button.ino
@@ -24,17 +24,18 @@ void buttonCallback(const char *label, ButtonState_t state) {
 }
 
 #define SCREEN_BUTTONS_X_START 5 // Gombok kezdő X koordinátája
+#define SCREEN_BUTTONS_Y 100
 #define SCREEN_BUTTON_HEIGHT 30
 #define SCREEN_BUTTON_WIDTH 70
 #define SCREEN_BUTTONS_GAP 10 // Define the gap between buttons
 #define BUTTON_X(n) (SCREEN_BUTTONS_X_START + (SCREEN_BUTTON_WIDTH + SCREEN_BUTTONS_GAP) * n)
 TftButton screenButtons[] = {
-    TftButton(&tft, BUTTON_X(0), 100, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Popup"), ButtonType::PUSHABLE, buttonCallback),
-    TftButton(&tft, BUTTON_X(1), 100, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Multi"), ButtonType::PUSHABLE, buttonCallback),
-    TftButton(&tft, BUTTON_X(2), 100, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Sw-1"), ButtonType::TOGGLE, buttonCallback),
-    TftButton(&tft, BUTTON_X(3), 100, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("FS List"), ButtonType::PUSHABLE, buttonCallback),
-    TftButton(&tft, BUTTON_X(4), 100, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Sw-2"), ButtonType::TOGGLE, buttonCallback, ButtonState::ON),
-    TftButton(&tft, BUTTON_X(5), 100, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Dis"), ButtonType::TOGGLE, buttonCallback) //
+    TftButton(&tft, BUTTON_X(0), SCREEN_BUTTONS_Y, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Popup"), ButtonType::PUSHABLE, buttonCallback),
+    TftButton(&tft, BUTTON_X(1), SCREEN_BUTTONS_Y, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Multi"), ButtonType::PUSHABLE, buttonCallback),
+    TftButton(&tft, BUTTON_X(2), SCREEN_BUTTONS_Y, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Sw-1"), ButtonType::TOGGLE, buttonCallback),
+    TftButton(&tft, BUTTON_X(3), SCREEN_BUTTONS_Y, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("FS List"), ButtonType::PUSHABLE, buttonCallback),
+    TftButton(&tft, BUTTON_X(4), SCREEN_BUTTONS_Y, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Sw-2"), ButtonType::TOGGLE, buttonCallback, ButtonState::ON),
+    TftButton(&tft, BUTTON_X(5), SCREEN_BUTTONS_Y, SCREEN_BUTTON_WIDTH, SCREEN_BUTTON_HEIGHT, F("Dis"), ButtonType::TOGGLE, buttonCallback) //
 };
 
 //---------------------------
